504091d5c3ff17960d5790a9ed9e1a031ff3b1a3
Konira-Robina <konirarobinam@gmail.com>
2026-05-13 07:50:34 +0300

    done with sales and stock pages

--- nyondoproject/core/models.py
+++ nyondoproject/core/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
     selling_price = models.IntegerField()
     stock_quantity = models.IntegerField()
 
-    def _str_(self):
+    def __str__(self):
         return self.name
 
 
@@ -19,81 +19,29 @@ class Customer(models.Model):
     nin = models.CharField(max_length=20)
     phone = models.CharField(max_length=15)
 
-    def _str_(self):
+    def __str__(self):
         return self.full_name
-
-
-# SUPPLIERS
-class Supplier(models.Model):
-    name = models.CharField(max_length=200)
-    contact = models.CharField(max_length=15)
-    credit_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-
-    def _str_(self):
-        return self.name
 
 
 # SALES
 class Sale(models.Model):
-
-    date = models.DateTimeField(auto_now_add=True)
-
-    sales_representative = models.CharField(max_length=200)
-
-    customer_name = models.CharField(max_length=200)
-
-    address = models.CharField(max_length=200)
-
-    phone_number = models.CharField(max_length=20)
-
-    customer_type = models.CharField(max_length=200)
-
-    product_name = models.ForeignKey(Product, on_delete=models.CASCADE)
-
-    quantity = models.IntegerField()
-
-    selling_price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-
-    transport_fee = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return f"{self.customer_name} - {self.product_name}"
-class Sale(models.Model):
-
     CUSTOMER_TYPES = (
         ('Retail', 'Retail'),
         ('Wholesale', 'Wholesale'),
     )
-
     date = models.DateTimeField(auto_now_add=True)
-
     sales_representative = models.CharField(max_length=200)
-
     customer_name = models.CharField(max_length=200)
-
     address = models.CharField(max_length=200)
-
     phone_number = models.CharField(max_length=20)
-
-    customer_type = models.CharField(
-        max_length=20,
-        choices=CUSTOMER_TYPES
-    )
-
+    customer_type = models.CharField( max_length=20,choices=CUSTOMER_TYPES)
     product_name = models.ForeignKey(Product, on_delete=models.CASCADE)
-
     quantity = models.IntegerField()
-
     selling_price = models.DecimalField(max_digits=10, decimal_places=2)
-
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-
     transport_fee = models.DecimalField(max_digits=10, decimal_places=2)
-
     def __str__(self):
-        return self.name
+        return self.product_name
 
 # CREDIT SCHEME
 class Deposit(models.Model):
@@ -104,8 +52,7 @@ class Deposit(models.Model):
 class CreditProduct(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def _str_(self):
+    def __str__(self):
         return self.name
     
 #class Deposit(models.Model):
@@ -123,11 +70,14 @@ class CreditPurchase(models.Model):
     total_cost = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
 
-    # Withdrawal (Picking Goods)
-
-class CreditPurchase(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    product = models.ForeignKey(CreditProduct, on_delete=models.CASCADE)
+class Supplier(models.Model):
+    Product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    supplier_name = models.CharField(max_length=100)
     quantity = models.IntegerField()
-    total_cost = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateTimeField(auto_now_add=True)
+    cost_price = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    method_of_payment = models.CharField(max_length=100,default="Cash")
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.name   
--- nyondoproject/core/views.py
+++ nyondoproject/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect,get_object_or_404
-from .models import Customer, Deposit, CreditProduct, CreditPurchase, Product, Sale 
+from .models import Customer, Deposit, CreditProduct, CreditPurchase, Product, Sale, Supplier
 from django.db.models import Sum
 from django.contrib import messages
 # Create your views here.
@@ -59,320 +59,108 @@ def edit_product(request, id):
         return redirect('products')
     return render(request, 'edit_product.html', {'product': product})
 
-
-
-
-
-
-    #product = Product.objects.all()
-
-
-
-    #if request.method == 'POST':
-            
-            #name=request.POST.get('name')
-            #category=request.POST.get('category')
-            #unit_cost=int(request.POST.get('unit_cost'))
-            #selling_price=int(request.POST.get('selling_price'))
-            #stock_quantity=int(request.POST.get('stock_quantity'))
-            #name=name,
-            #category=category,
-            #unit_cost=unit_cost,
-            #selling_price=selling_price,
-            #stock_quantity=stock_quantity
-            #product.save()
-               
-           
-    
-            
-
-            
-           # if new_product:
-#Edit product
-               # new_product = Product()
-               # new_product.name = name
-                #new_product.category = category
-                #new_product.unit_cost = unit_cost
-                #new_product.selling_price = selling_price
-                #new_product.stock_quantity = stock_quantity
-                #new_product.save()
-            #else:#ADD
-               # product.objects.create(
-                #    name=name,
-                   # category=category,
-                   # unit_cost=unit_cost,
-                    #selling_price=selling_price,
-                   # stock_quantity=stock_quantity
-               # )
-                
 def delete_product(request, id):
-
     product = get_object_or_404(Product, id=id)
-
     product.delete()
-
-    messages.success(request, "Product deleted successfully!")
-
     return redirect('products')
+#return render(request, 'delete_product.html')
 
+# SALES INFORMATION
 
- #SALES
- # from django.shortcuts import render, redirect
-from .models import Product, Sale
-
-
-def sales_page(request):
-
+# ADD SALE VIEW
+def add_sales(request):
+    # Fetching all products from the model class
     products = Product.objects.all()
-
-    if request.method == 'POST':
-
+    if request.method == 'POST': # Checking if form is submitted
+        # Geting form data
         sales_representative = request.POST.get('sales_representative')
-
         customer_name = request.POST.get('customer_name')
-
         address = request.POST.get('address')
-
         phone_number = request.POST.get('phone_number')
-
         customer_type = request.POST.get('customer_type')
-
         product_id = request.POST.get('product_name')
-
         quantity = int(request.POST.get('quantity'))
-
         selling_price = float(request.POST.get('selling_price'))
-
         transport_fee = float(request.POST.get('transport_fee'))
-
-        # Get product
+        # Geting selected product
         product = Product.objects.get(id=product_id)
+        
+        # STOCK VALIDATION
+        # Replacing stock_quantity with the actual product stock field
+        if quantity > product.stock_quantity:
+            messages.error(request,"Not enough stock available")
+            return redirect('add_sales')
 
-        # Calculate total
-        total_amount = (
-            quantity * selling_price
-        ) + transport_fee
-
-        # Save sale
+        # CALCULATIng THE TOTAL
+        total_amount = (quantity * selling_price) + transport_fee
+       
+        # SAVING SALE
         Sale.objects.create(
-
             sales_representative=sales_representative,
-
             customer_name=customer_name,
-
             address=address,
-
             phone_number=phone_number,
-
             customer_type=customer_type,
-
             product_name=product,
-
             quantity=quantity,
-
             selling_price=selling_price,
-
             total_amount=total_amount,
-
             transport_fee=transport_fee
         )
 
-        # Reduce stock
-        product.quantity -= quantity
-
+        # REDUCING STOCK
+        product.stock_quantity -= quantity
         product.save()
 
-        return redirect('sales_page')
+        # Message to show up after adding
+        messages.success(request,"Sale added successfully")
+        return redirect('sales')
+    # GET REQUEST
+    context = {'products': products}
+    return render(request,'add_sales.html',context)
 
-    context = {
-        'products': products
-    }
+# SALES LIST VIEW
+def sales(request):
+    # Fetch all sales
+    sales = Sale.objects.all().order_by('-date')
+    context = {'sales': sales}
+    return render( request,'sales.html',context)
 
-    return render(request,'sales.html',context) 
-
-def sales_page(request):
-
-    products = Product.objects.all()
-
-    sales = Sale.objects.all()
-
-    if request.method == 'POST':
-
-        sales_representative = request.POST.get('sales_representative')
-
-        customer_name = request.POST.get('customer_name')
-
-        address = request.POST.get('address')
-
-        phone_number = request.POST.get('phone_number')
-
-        customer_type = request.POST.get('customer_type')
-
-        product_id = request.POST.get('product_name')
-
-        quantity = int(request.POST.get('quantity'))
-
-        selling_price = float(request.POST.get('selling_price'))
-
-        transport_fee = float(request.POST.get('transport_fee'))
-
-        product = Product.objects.get(id=product_id)
-
-        # VALIDATION
-        if quantity > product.quantity:
-
-            messages.error(
-                request,
-                "Not enough stock available"
-            )
-
-            return redirect('sales_page')
-
-        total_amount = (
-            quantity * selling_price
-        ) + transport_fee
-
-        Sale.objects.create(
-
-            sales_representative=sales_representative,
-
-            customer_name=customer_name,
-
-            address=address,
-
-            phone_number=phone_number,
-
-            customer_type=customer_type,
-
-            product_name=product,
-
-            quantity=quantity,
-
-            selling_price=selling_price,
-
-            total_amount=total_amount,
-
-            transport_fee=transport_fee
-        )
-
-        # Reduce Stock
-        product.quantity -= quantity
-
-        product.save()
-
-        messages.success(
-            request,
-            "Sale recorded successfully"
-        )
-
-        return redirect('sales_page')
-
-    context = {
-
-        'products': products,
-
-        'sales': sales
-
-    }
-
-    return render(
-        request,
-        'sales.html',
-        context
-    )
- 
+# EDIT SALE
 def edit_sale(request, id):
-
     sale = get_object_or_404(Sale, id=id)
-
     products = Product.objects.all()
-
     if request.method == 'POST':
-
-        sale.sales_representative = request.POST.get(
-            'sales_representative'
-        )
-
-        sale.customer_name = request.POST.get(
-            'customer_name'
-        )
-
-        sale.address = request.POST.get(
-            'address'
-        )
-
-        sale.phone_number = request.POST.get(
-            'phone_number'
-        )
-
-        sale.customer_type = request.POST.get(
-            'customer_type'
-        )
-
-        product_id = request.POST.get(
-            'product_name'
-        )
-
-        sale.product_name = Product.objects.get(
-            id=product_id
-        )
-
-        sale.quantity = request.POST.get(
-            'quantity'
-        )
-
-        sale.selling_price = request.POST.get(
-            'selling_price'
-        )
-
-        sale.transport_fee = request.POST.get(
-            'transport_fee'
-        )
-
-        sale.total_amount = (
-            float(sale.quantity)
-            * float(sale.selling_price)
-        ) + float(sale.transport_fee)
-
+        sale.sales_representative = request.POST.get('sales_representative')
+        sale.customer_name = request.POST.get('customer_name')
+        sale.address = request.POST.get('address')
+        sale.phone_number = request.POST.get('phone_number')
+        sale.customer_type = request.POST.get('customer_type')
+        product_id = request.POST.get('product_name')
+        product = get_object_or_404( Product, id=product_id)
+        sale.product_name = product
+        sale.quantity = request.POST.get('quantity')
+        sale.selling_price = request.POST.get('selling_price')
+        sale.transport_fee = request.POST.get('transport_fee')
+        sale.total_amount = (float(sale.quantity)* float(sale.selling_price)) + float(sale.transport_fee)
         sale.save()
+        messages.success(request,"Sale updated successfully")
+        return redirect('sales')
 
-        messages.success(
-            request,
-            "Sale updated successfully"
-        )
+    context = {'sale': sale}
+    return render(request,'edit_sales.html',context)
 
-        return redirect('sales_page')
-
-    context = {
-
-        'sale': sale,
-
-        'products': products
-
-    }
-
-    return render(
-        request,
-        'edit_sale.html',
-        context
-    )
+#DELETING
 def delete_sale(request, id):
-
     sale = get_object_or_404(Sale, id=id)
-
     sale.delete()
+    messages.success(request,"Sale deleted successfully")
+    return redirect('sales')
 
-    messages.success(
-        request,
-        "Sale deleted successfully"
-    )
-
-    return redirect('sales_page')
-
+# CUSTOMER DETAILS
 def customers(request):
     data = Customer.objects.all()
     return render(request, 'customers.html', {'customers': data})
-
 
 def add_customer(request):
     if request.method == 'POST':
@@ -382,7 +170,6 @@ def add_customer(request):
             phone=request.POST['phone']
         )
         return redirect('customers')
-
     return render(request, 'add_customer.html')
 
 def credit_purchase(request):
@@ -407,14 +194,50 @@ def credit_purchase(request):
             quantity=qty,
             total_cost=total_cost
         )
-
         return redirect('credit_purchase')
-
     return render(request, 'credit_purchase.html', {
         'customers': customers,
         'products': products
     })
 
+def add_supplier(request):
+    #fetching all supplier details from the class
+    supplier=Supplier.objects.all()
+    if request.method == 'POST': # if form is submitted then get for me these details
+        product_id = request.POST.get('stock')
+        supplier_name = request.POST.get('supplier_name')
+        quantity = int(request.POST.get('quantity'))
+        cost_price = float(request.POST.get('cost_price'))
+        date = request.POST.get('date')
+        payment_method = request.POST.get('payment_method')
+        amount_paid = float(request.POST.get('amount_paid'))
 
+        supplier=Product.objects.get(id=product_id)
+        total_cost = quantity * cost_price
+        
+        if payment_method != 'credit':
+            amount_paid = total_cost
+
+        
+        #supplier=get_object_or_404(
+        #product_id = product
+        #supplier_name = supplier_name
+        #quantity = quantity
+        #cost_price = cost_price
+        #date = date
+        #payment_method = payment_method
+        #amount_paid = amount_paid)
+       # supplier.save()
+
+        #quantity += quantity
+        #products.save()
+
+        return redirect('supplier')
+    return render(request, 'add_supplier.html', {'products': products})
+
+
+def supplier(request):
+    supply = Supplier.objects.all()
+    return render(request, 'supplier.html', {'supply': supply})
 
 
